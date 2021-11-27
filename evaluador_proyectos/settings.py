1cd8df6df409a27e27a4b56b7c2f4fdbba3eb19c
Luis Mejias <lonnath.dev@gmail.com>
2021-11-27 01:23:17 -0500

    Avances segunda entrega.

--- evaluador_proyectos/settings.py
+++ evaluador_proyectos/settings.py
@@ -137,13 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'build/static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/staticfiles/')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
